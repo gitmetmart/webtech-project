cdc0e687df1127fabe25cf613abf8b88ff815223
M@rt <123403311+gitmetmart@users.noreply.github.com>
2024-03-13 13:09:08 +0100

    update function 2

--- source/views.py
+++ source/views.py
@@ -10,9 +10,9 @@ views = Blueprint('views', __name__)
 @login_required
 def home():
     """
-    Deze functie handelt het verzoek af voor de homepagina van de applicatie.
-    Als het verzoek een POST-verzoek is, wordt er een nieuwe notitie toegevoegd aan de database.
-    Als het verzoek een GET-verzoek is, wordt de homepagina gerenderd met de ingelogde gebruiker.
+        Deze functie handelt het verzoek af voor de homepagina van de applicatie.
+        Als het verzoek een POST-verzoek is, wordt er een nieuwe notitie toegevoegd aan de database.
+        Als het verzoek een GET-verzoek is, wordt de homepagina gerenderd met de ingelogde gebruiker.
     """
     if request.method == 'POST': 
         note = request.form.get('note') #Vraagt de notes op
@@ -27,15 +27,22 @@ def home():
 
     return render_template("home.html", user=current_user)  #Returnt de home pagine + ingeloggede user
 
-
-@views.route('/update_note', methods=['POST'])
-def update_note_endpoint():
-    note_id = request.form['id']
-    new_data = request.form['data']
-    success = update_note(note_id, new_data)
-    return {'success': success}
-
-    return jsonify({})
+@views.route('/update-note', methods=['POST'])
+def update_note(note_id, new_data):
+    """
+        Deze functie handelt het verzoek af om een notitie bij te werken.
+        Het verzoek bevat de ID van de notitie die moet worden bijgewerkt en de nieuwe gegevens voor de notitie.
+        Als de notitie bestaat en de gebruiker is de eigenaar van de notitie, wordt de notitie bijgewerkt in de database.
+    """
+    note = json.loads(request.data) #Vraag het Index.html java script bestand op (Index.js)
+    noteId = note['noteId']
+    new_data = note['newData']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id: #checkt of de user id's gelijkt staat aan het user note id voor dat er een update mag worden uitgevoerd
+            note.data = new_data
+            db.session.commit()
+    return True
 
 @views.route('/delete-note', methods=['POST'])
 def delete_note():  
